afcf6acab365edac412bfcc8023e30cc4ae9faca
JameNori <jame_nori@hotmail.com>
2025-07-16 15:52:37 +0700

    fix: update address and adjust pricing variables in ex-1and2.js

--- ex-1and2.js
+++ ex-1and2.js
@@ -8,3 +8,7 @@ let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
 const restaurantName = "Chef's Table by Chef James"
 const totalNetPrice = 15000
 const activeRestaurantCount = 15000
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+totalNetPrice = 5000
+activeRestaurantCount = 2500
